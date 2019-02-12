2a34c4e085b900fdb9ab5117376a9056eb169b54
anan0110692 <46865587+anan0110692@users.noreply.github.com>
2019-02-12 12:31:52 +0000

    Update Train_.m

--- Train_.m
+++ Train_.m
@@ -1,15 +1,18 @@
 function Train_(trainloc,testloc,outloc)
-adjustsize(trainloc) %adjusting the training images size 
+%adjusting the training images size 
+adjustsize(trainloc) 
+
 % Load Data
-%
 % automatically labels the images based on folder names and stores the data
 % as an |ImageDatastore| object.
 
 images=imageDatastore(trainloc,'IncludeSubfolders',true,...
     'LabelSource','foldernames');
+
 % Divide the data into training and validation data sets. Use 70% of the
 % images for training and 30% for validation. 
 [trainingImages] = splitEachLabel(images,1,'randomized');
+
 %Load Pretrained Network
 % Load the pretrained AlexNet neural network. 
 net = alexnet;
@@ -21,7 +24,7 @@ net = alexnet;
 % classification problem. 
 layersTransfer = net.Layers(1:end-3);
 
-%
+
 % Transfer the layers to the new classification task by replacing the last
 % three layers with a fully connected layer, a softmax layer, and a
 % classification output layer. Specify the options of the new fully
@@ -72,5 +75,9 @@ options = trainingOptions('sgdm',...
 
 % Train the network that consists of the transferred and new layers. 
 netTransfer = trainNetwork(trainingImages,layers,options);
-changeTestsize_(testloc); %adjust the testing images size.
-resulting_(outloc,testloc,netTransfer);  %write the classification result of the testing data on txt file called RESULT.
+
+%adjust the testing images size.
+changeTestsize_(testloc); 
+
+%write the classification result of the testing data on txt file called RESULT.
+resulting_(outloc,testloc,netTransfer);  
