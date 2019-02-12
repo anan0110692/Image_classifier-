37009a087af4df529f2a5cd5052d6b9323bd312b
anan0110692 <46865587+anan0110692@users.noreply.github.com>
2019-02-12 12:26:41 +0000

    Update changeTestsize_.m

--- changeTestsize_.m
+++ changeTestsize_.m
@@ -3,6 +3,5 @@ Testfolder=fullfile('C:\Users\Abo_Alon\Desktop\New folder (3)'); %Location of th
     Testsetofimages=dir(fullfile(Testfolder,'*.jpg')); %Getting the names of all the images inside Testfolder  and save it in Testsetofimages struct-type array
     for m=1:size(Testsetofimages,1)                    %looping through the test images
         x=imread(fullfile(Testfolder,Testsetofimages(m).name)); %loading the image in the m row of Testsetofimages and save it to dummy variable x
-%        x=cat(3,x,x,x);                                         %Represent the the gray scale as RGB image with 3 same layers.
      imwrite(imresize(x,[227 227]),fullfile(Testfolder,Testsetofimages(m).name)); %change the the size of the image and write it to the same file that it has been loaded from.
     end %end of the foor loop.
